1e00a91f33a0d0e7bc261104717cfa4f4b60a66c
chaewonkim <kimlove9182@gmail.com>
2024-03-24 21:33:36 +0900

    :sparkles: feat: 챌린지 뽑기 구현

---
+++ src/main/java/com/example/TCC/repository/TryChallRepository.java
@@ -0,0 +1,14 @@
+package com.example.TCC.repository;
+
+import com.example.TCC.domain.Member;
+import com.example.TCC.domain.TryChall;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TryChallRepository extends JpaRepository<TryChall, Long> {
+
+    boolean existsByMemberAndExpireTimeIsNotNull(Member member);
+
+    Optional<TryChall> findByMemberAndExpireTimeIsNull(Member member);
+}
