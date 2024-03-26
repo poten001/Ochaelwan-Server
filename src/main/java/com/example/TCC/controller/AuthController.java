ff421b071091057ee21b2df654bcedd127494a65
chaewonkim <kimlove9182@gmail.com>
2024-03-26 12:22:44 +0900

    :recycle: refactor: 주석 추가

--- src/main/java/com/example/TCC/controller/AuthController.java
+++ src/main/java/com/example/TCC/controller/AuthController.java
@@ -1,22 +1,25 @@
 package com.example.TCC.controller;
 
+import com.example.TCC.kakao.KakaoApiClient;
 import com.example.TCC.kakao.KakaoLoginParams;
 import com.example.TCC.kakao.jwt.AuthTokens;
 import com.example.TCC.service.OAuthLoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
     private final OAuthLoginService oAuthLoginService;
+    private final KakaoApiClient kakaoApiClient;
 
-    @PostMapping("/kakao")
+    //카카오 로그인
+    @PostMapping("/login")
     public ResponseEntity<AuthTokens> loginKakao(@RequestBody KakaoLoginParams params) {
         return ResponseEntity.ok(oAuthLoginService.login(params));
     }
