a73e0e182def5f41966c6a9603dbce75a7c53b5a
chaewonkim <kimlove9182@gmail.com>
2024-03-26 12:22:35 +0900

    :recycle: refactor: 토큰 관련 파일 리팩토링

--- src/main/java/com/example/TCC/kakao/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/TCC/kakao/jwt/JwtTokenProvider.java
@@ -1,10 +1,6 @@
 package com.example.TCC.kakao.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,38 +8,38 @@ import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
 
-@Component
-public class JwtTokenProvider {
+    @Component
+    public class JwtTokenProvider {
+        private final Key key;
 
-    private final Key key;
+        public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey) {
+            byte[] keyBytes = Keys.secretKeyFor(SignatureAlgorithm.HS512).getEncoded();
+            this.key = Keys.hmacShaKeyFor(keyBytes);
+        }
 
-    public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey) {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        this.key = Keys.hmacShaKeyFor(keyBytes);
-    }
+        public String generateToken(String subject, long validityPeriodMilliseconds) {
+            Date now = new Date();
+            Date validity = new Date(now.getTime() + validityPeriodMilliseconds);
 
-    public String generate(String subject, Date expiredAt) {
-        return Jwts.builder()
-                .setSubject(subject)
-                .setExpiration(expiredAt)
-                .signWith(key, SignatureAlgorithm.HS512)
-                .compact();
-    }
+            return Jwts.builder()
+                    .setSubject(subject)
+                    .setIssuedAt(now)
+                    .setExpiration(validity)
+                    .signWith(key, SignatureAlgorithm.HS512)
+                    .compact();
+        }
 
-    public String extractSubject(String accessToken) {
-        Claims claims = parseClaims(accessToken);
-        return claims.getSubject();
-    }
+        public boolean validateToken(String token) {
+            try {
+                Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+                return true;
+            } catch (JwtException | IllegalArgumentException e) {
+                return false;
+            }
+        }
 
-    private Claims parseClaims(String accessToken) {
-        try {
-            return Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(accessToken)
-                    .getBody();
-        } catch (ExpiredJwtException e) {
-            return e.getClaims();
+        public String getSubject(String token) {
+            Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+            return claims.getSubject();
         }
     }
-}
