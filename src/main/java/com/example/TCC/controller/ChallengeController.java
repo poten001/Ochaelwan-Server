f8e84212f8726e3d5113033a47850ec91c444243
chaewonkim <kimlove9182@gmail.com>
2024-03-27 15:20:47 +0900

    :sparkles: feat: 완료한 챌린지 전체 조회 구현

--- src/main/java/com/example/TCC/controller/ChallengeController.java
+++ src/main/java/com/example/TCC/controller/ChallengeController.java
@@ -5,6 +5,7 @@ import com.example.TCC.dto.request.DrawChallengeRequestDto;
 import com.example.TCC.dto.response.CompleteChallengeResponseDto;
 import com.example.TCC.dto.response.DrawChallengeResponseDto;
 import com.example.TCC.dto.response.TryChallengeResponseDto;
+import com.example.TCC.exception.NotFoundException;
 import com.example.TCC.service.ChallengeService;
 import com.example.TCC.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -12,16 +13,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/challenge")
 public class ChallengeController {
 
     private final MemberService memberService;
     private final ChallengeService challengeService;
 
     //챌린지 뽑기
-    @PostMapping("/draw")
+    @PostMapping("/challenge/draw")
     public ResponseEntity<DrawChallengeResponseDto> draw(@RequestBody DrawChallengeRequestDto dto, @RequestHeader("Authorization") String authorizationHeader) {
         //로그인한 사용자가 있는지 확인
         Member member = memberService.getCurrentUser(authorizationHeader);
@@ -31,7 +33,7 @@ public class ChallengeController {
     }
 
     //챌린지 도전
-     @PostMapping("/try")
+     @PostMapping("/challenge/try")
      public ResponseEntity<?> tryChallenge(@RequestHeader("Authorization") String authorizationHeader) {
          //로그인한 사용자가 있는지 확인
          Member member = memberService.getCurrentUser(authorizationHeader);
@@ -41,7 +43,7 @@ public class ChallengeController {
     }
 
     //챌린지 도전항목 조회 (오챌완 버튼도 이걸로)
-    @GetMapping("/try/check")
+    @GetMapping("/challenge/try/check")
     public ResponseEntity<TryChallengeResponseDto> tryCheck(@RequestHeader("Authorization") String authorizationHeader) {
         //로그인한 사용자가 있는지 확인
         Member member = memberService.getCurrentUser(authorizationHeader);
@@ -51,7 +53,7 @@ public class ChallengeController {
     }
 
     //챌린지 완료
-    @PostMapping("/complete")
+    @PostMapping("/challenge/complete")
     public ResponseEntity<CompleteChallengeResponseDto> complete(@RequestHeader("Authorization") String authorizationHeader) {
         //로그인한 사용자가 있는지 확인
         Member member = memberService.getCurrentUser(authorizationHeader);
@@ -61,6 +63,15 @@ public class ChallengeController {
     }
 
     //완료한 챌린지 전체 조회
+    @GetMapping("/challenges")
+    public ResponseEntity<List<CompleteChallengeResponseDto>> showAll(@RequestHeader("Authorization") String authorizationHeader) {
+        //로그인한 사용자가 있는지 확인
+        Member member = memberService.getCurrentUser(authorizationHeader);
+        List<CompleteChallengeResponseDto> dtos = challengeService.showAll(member);
 
-    //완료한 챌린지 상세 조회
+        if (dtos == null) {
+            throw new NotFoundException("완료한 챌린지가 없습니다.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
 }
--- src/main/java/com/example/TCC/repository/CompleteChallRepository.java
+++ src/main/java/com/example/TCC/repository/CompleteChallRepository.java
@@ -3,5 +3,11 @@ package com.example.TCC.repository;
 import com.example.TCC.domain.CompleteChall;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CompleteChallRepository extends JpaRepository<CompleteChall, Long> {
+    List<CompleteChall> findAllByMemberId(Long memberId);
+
+    Optional<CompleteChall> findByIdAndMemberId(Long challengeId, Long memberId);
 }
