2fa8c548b243c1d11063436f7fbd3629ba0360ef
chaewonkim <kimlove9182@gmail.com>
2024-03-31 20:02:07 +0900

    :recycle: refactor: corsConfig에 localhost 추가

--- src/main/java/com/example/TCC/config/CorsConfig.java
+++ src/main/java/com/example/TCC/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://ochaelwan.vercel.app"));
+        config.setAllowedOrigins(List.of("https://ochaelwan.vercel.app", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
